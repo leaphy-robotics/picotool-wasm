5221cf24d867a4dad4fbe373ae734433c0d98e32
sverben <59171289+sverben@users.noreply.github.com>
2024-03-21 22:48:28 +0100

    fix: ewww

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsup'
+import replace from 'esbuild-plugin-replace-regex'
 
 export default defineConfig({
     target: 'es2020',
@@ -6,5 +7,16 @@ export default defineConfig({
     splitting: false,
     sourcemap: true,
     clean: true,
-    dts: true
+    dts: true,
+    esbuildOptions(options) {
+        if (!options.define) options.define = {}
+        options.define['import.meta'] = '{}'
+    },
+    esbuildPlugins: [
+        replace({
+            patterns: [
+                [/ENVIRONMENT_IS_NODE(?! =)/g, 'false'],
+            ]
+        })
+    ]
 })
